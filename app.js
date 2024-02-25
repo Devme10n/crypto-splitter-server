2b568835b866dda69f9d6c1310e41d911d1aa11b
max990624 <max990624@naver.com>
2024-02-25 21:55:38 +0900

    feat: 10. 파일 정렬, 11. 파일 병합 기능 추가
    - 테스트를 위해 9. 분할된 파일 다운로드 미구현
    - 하드코딩을 통해 기능 테스트(사용자 입력 부분)
    - 6. 분할된 파일 업로드 주석처리 + internet 폴더로 파일들 이동으로 대체

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const { changeFilename, encryptAndSplitFile } = require('./file-processing/writer');
 const { renameFilesAndCreateMapping, uploadFiles, saveMappingDataJsonPostgreSQL } = require('./file-processing/middle');
-const { getFileMappingInfo } = require('./file-processing/reader');
+const { getFileMappingInfo, sortFiles, mergeFiles } = require('./file-processing/reader');
+const { compareFileHash } = require('./utils/comparefile');
+
+// 하드코딩
+const fs = require('fs').promises;
+const newLocation = '/Users/mac/Documents/split_file/internet';
 
 // 업로드 된 파일 위치
 const folderPath = path.join(__dirname, 'uploadfile');
@@ -23,35 +28,88 @@ const processFiles = async () => {
 
     // 파일 이름 변경 및 매핑 생성
     const { renamedFilePaths, splitFileOrderMapping, desEncryptedFileName } = await renameFilesAndCreateMapping(originalFileNames, splitFilesPath);
-    console.log('파일 이름 변경 및 매핑 정보 생성이 완료되었습니다.');
+    console.log('파일 이름 변경 및 매핑 정보 생성 완료.');
 
-    // 파일 업로드
-    const uploadUrl = 'http://localhost:3000/upload'; // 업로드할 URL 예시
-    await uploadFiles(renamedFilePaths, uploadUrl);
-    console.log('파일 업로드가 완료되었습니다.');
+    //---------------------------------------------------------
+    // 잠시 테스트를 위해 주석 처리
+    //---------------------------------------------------------
+    // // 파일 업로드
+    // const uploadUrl = 'http://localhost:3000/upload'; // 업로드할 URL 예시
+    // await uploadFiles(renamedFilePaths, uploadUrl);
+    // console.log('파일 업로드가 완료되었습니다.');
 
+    // 하드코딩
+    /**
+     * 주어진 파일 경로 목록의 파일들을 새 위치로 이동.
+     * @param {string[]} filePaths - 이동할 파일들의 경로.
+     * @param {string} newLocation - 파일들을 이동할 새 위치.
+     */
+    const moveFilesToNewLocation = async (filePaths, newLocation) => {
+      const movedFilePaths = [];
+      try {
+          // parentFolderPath: 모든 파일의 상위 폴더
+          const parentFolderPath = path.dirname(filePaths[0]);
+
+          // 새 위치에 상위 폴더를 생성.
+          const newParentFolderPath = path.join(newLocation, path.basename(parentFolderPath));
+          await fs.mkdir(newParentFolderPath, { recursive: true });
+
+          for (const filePath of filePaths) {
+              const fileName = path.basename(filePath);
+              const newFilePath = path.join(newParentFolderPath, fileName);
+              await fs.rename(filePath, newFilePath);
+              movedFilePaths.push(newFilePath); // 배열에 이동된 파일 경로 추가
+          }
+          return movedFilePaths; // 이동된 파일 경로들의 배열 반환
+      } catch (error) {
+          console.error('파일을 이동하는 도중 오류 발생:', error);
+      }
+    };
+  
+    const movedFilePaths = await moveFilesToNewLocation(renamedFilePaths, newLocation);
+    //
+
+    
     // 업로드 후 매핑 데이터 저장
     await saveMappingDataJsonPostgreSQL(desEncryptedFileName, splitFileOrderMapping);
     // 아래 console.log는 실패해도 성공했다고 함.
     //console.log('매핑 데이터가 성공적으로 저장되었습니다.');
+
+    // 하드 코딩
+    return movedFilePaths;
+
+
   } catch (error) {
     console.error('파일 처리 중 오류가 발생했습니다:', error);
   }
 };
 
-const afterProcessFiles = async () => {
+const afterProcessFiles = async (movedFilePaths) => {
   try {
     // getFileMappingInfo 함수 실행
-    const encryptedFilename = 'f2ba300a28fa225152c59332cec8e166';
+    // 하드 코딩
+    const encryptedFilename = 'f2ba300a28fa225152c59332cec8e166'; // 사용자가 입력한 파일명을 대칭키로 암호화
     const db_mappingInfo = await getFileMappingInfo(encryptedFilename);
     // console.log('파일 매핑 정보:', db_mappingInfo);
+
+    const sortedFiles = sortFiles(movedFilePaths, db_mappingInfo)
+    // console.log('정렬된 파일들:', sortedFiles);
+
+    // 하드코딩
+    const outputPath = '/Users/mac/Documents/split_file/output';
+    const mergedFilePath = await mergeFiles(sortedFiles, outputPath, encryptedFilename)
+    // console.log(mergedFilePath)
+    // console.log(folderPath)
+
+    // 하드코딩
+    const areFileSame = await compareFileHash("/Users/mac/Documents/split_file/uploadfile/f2ba300a28fa225152c59332cec8e166", mergedFilePath)
   } catch (error) {
     console.error('파일 매핑 정보 조회 중 오류 발생:', error);
   }
 };
 
 processFiles()
-  .then(afterProcessFiles)
+  .then(movedFilePaths => afterProcessFiles(movedFilePaths))
   .catch(error => {
     console.error('오류 발생:', error);
   });
--- file-processing/reader.js
+++ file-processing/reader.js
@@ -1,5 +1,12 @@
-const { logError } = require('../utils/logger'); // logError 함수 import
+const fs = require('fs').promises;
+const axios = require('axios');
+const path = require('path');
+const splitFile = require('split-file');
+
+const { logger } = require('../utils/logger');
 const FileMappingJson = require('../models/postgreSQLModels');
+const { isDirectory } = require('../utils/pathUtils');
+
 
 //=============================================================================================
 // 파일 병합
@@ -8,6 +15,11 @@ const FileMappingJson = require('../models/postgreSQLModels');
 //---------------------------------------------------------
 // 8. fileOrderMapping 정보 조회
 //---------------------------------------------------------
+/**
+ * 주어진 암호화된 파일명에 대한 파일 매핑 정보를 검색합니다.
+ * @param {string} encryptedFilename - 검색할 암호화된 파일의 이름
+ * @returns {Promise<Object|null>} 매핑 정보 객체를 반환하는 프로미스
+ */
 async function getFileMappingInfo(encryptedFilename) {
     try {
         const mappingInfo = await FileMappingJson.findOne({
@@ -15,10 +27,11 @@ async function getFileMappingInfo(encryptedFilename) {
         });
 
         if (mappingInfo) {
+            // console.log("잘 통과함")
             return mappingInfo.mapping_info;
         } else {
             console.log(`${encryptedFilename} 파일 존재하지 않음`)
-            return null; // 해당 파일이 존재하지 않을 경우 처리
+            return null;
         }
     } catch (error) {
         console.error('파일 매핑 정보 조회 중 오류 발생:', error);
@@ -30,29 +43,59 @@ async function getFileMappingInfo(encryptedFilename) {
 // 9. 분할된 파일들을 다운로드 (아직 구현되지 않음)
 // Promise.all()을 사용하여 병렬로 다운로드
 //---------------------------------------------------------
+/**
+ * (테스트 안해봄) 주어진 URL에서 파일을 다운로드하고 지정된 폴더에 저장합니다.
+ * @param {string[]} urls - 다운로드할 파일의 URL 배열입니다.
+ * @param {string} destFolder - 파일이 저장될 대상 폴더입니다.
+ * @returns {Promise<string[]>} 다운로드된 파일의 경로 배열로 resolve되는 프로미스입니다.
+ */
+async function downloadFiles(urls, destFolder) {
+    try {
+        const downloadedFilePaths = await Promise.all(urls.map(async (url, index) => {
+            const fileName = path.basename(url); // URL에서 파일 이름 추출
+            const filePath = path.join(destFolder, `${index}-${fileName}`); // 파일 이름 앞에 인덱스를 추가하여 고유한 파일 이름을 보장합니다.
+            const response = await axios({ url, responseType: 'stream' });
+            const writer = fs.createWriteStream(filePath);
+            
+            response.data.pipe(writer);
 
-//#############################################################################################
-// 분할된 파일들의 순서가 매핑된 순서와 일치한 상태여서 sort 함수를 사용하지 않아도 파일 병합이 가능함. -> 수정필요
-//#############################################################################################
+            return new Promise((resolve, reject) => {
+                writer.on('finish', () => resolve(filePath));
+                writer.on('error', reject);
+            });
+        }));
+
+        console.log('모든 파일이 성공적으로 다운로드되었습니다.');
+        return downloadedFilePaths;
+    } catch (error) {
+        console.error('파일 다운로드에 실패했습니다:', error);
+        throw error; // 호출자가 처리할 수 있도록 에러를 다시 던집니다.
+    }
+}
+
 //---------------------------------------------------------
 // 10. 파일 정렬
 //---------------------------------------------------------
-function sortFiles(splitFileNames, splitFileOrderMapping) {
-    // 분할된 파일들을 원래대로 정렬
-    splitFileNames.sort((a, b) => splitFileOrderMapping[a] - splitFileOrderMapping[b]);
-    return splitFileNames;
-}
+const sortFiles = (filesPath, mappingInfo) => {
+    // 다운로드 받은 파일들을 mappingInfo를 기반으로 정렬
+    const sortedFiles = filesPath.sort((a, b) => mappingInfo[a] - mappingInfo[b]);
+
+    return sortedFiles;
+};
 
 //---------------------------------------------------------
 // 11. 파일 병합
 //---------------------------------------------------------
-async function mergeFiles(sortedFileNames, outputPath) {
+async function mergeFiles(filesPath, outputPath, mergedFileName) {
+    const mergedFilePath = path.join(outputPath, mergedFileName);
     try {
         // 파일 합치기
-        await splitFile.mergeFiles(sortedFileNames, outputPath);
+        await splitFile.mergeFiles(filesPath, mergedFilePath);
         console.log('파일 합치기 완료');
+        return mergedFilePath;
     } catch(err) {
-        logError(err);
+        console.error('파일 병합 도중 오류 발생:', err);
+        throw err;
     }
 }
 
