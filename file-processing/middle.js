20f4f50c9287e907d04457eb46c80ca1fd7bdc7e
max990624 <max990624@naver.com>
2024-03-12 22:18:41 +0900

    refactor: saveMappingDataJsonPostgreSQL 함수에 encryptedSymmetricKey 매개변수 추가
    - encryptedSymmetricKey: 공개키로 암호화된 대칭키
    - buffer를 base64로 인코딩, db에 저장
    - db 모델 정의 변경

--- file-processing/middle.js
+++ file-processing/middle.js
@@ -96,12 +96,16 @@ async function uploadFiles(files, uploadUrl) {
  * @param {string} desEncryptedFileName - 대칭키로 암호화된 파일명
  * @param {Object} mappingInfo - { uuid : original_index } 형태의 uuid로 변경된 파일명 매핑 정보
  */
-const saveMappingDataJsonPostgreSQL = async (desEncryptedFileName, mappingInfo) => {
+const saveMappingDataJsonPostgreSQL = async (desEncryptedFileName, mappingInfo, encryptedSymmetricKey) => {
     try {
+        // 암호화된 대칭키(Buffer)를 base64로 인코딩
+        const encryptedSymmetricKeyBase64 = Buffer.from(encryptedSymmetricKey).toString('base64');
+
         // FileMappingJson 모델을 사용하여 새 레코드를 생성, 데이터베이스에 저장.
         await FileMappingJson.create({
             encrypted_filename: desEncryptedFileName, // 데이터베이스에 저장될 암호화된 파일명
-            mapping_info: mappingInfo // 분할된 파일과 원본 순서 사이의 매핑 정보
+            mapping_info: mappingInfo, // 분할된 파일과 원본 순서 사이의 매핑 정보\
+            encrypted_symmetric_key: encryptedSymmetricKeyBase64 // 공개키로 암호화된 대칭키
         });
         console.log('PostgreSQL에 매핑 데이터가 JSON 형식으로 성공적으로 저장되었습니다.');
     } catch (error) {
--- models/postgreSQLModels.js
+++ models/postgreSQLModels.js
@@ -15,14 +15,18 @@ const FileMappingJson = sequelize.define('file_mapping_json', {
     mapping_info: {
         type: DataTypes.JSONB,
         allowNull: false
+    },
+    encrypted_symmetric_key: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 });
 
 // 테이블 생성
 FileMappingJson.sync({ force: true }).then(() => {
-    console.log('테이블이 성공적으로 생성되었습니다.');
+    console.log('테이블이 성공적으로 업데이트되었습니다.');
 }).catch(err => {
-    console.error('테이블 생성 중 오류 발생:', err);
+    console.error('테이블 업데이트 중 오류 발생:', err);
 });
 
 module.exports = FileMappingJson;
