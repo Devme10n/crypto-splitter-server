f16e5a2f6bb60f1292516f4718f77e0d2a56196b
max990624 <max990624@naver.com>
2024-03-22 18:05:36 +0900

    feat: 파일 병합, 파일 정렬(fix), 파일 복호화 기능 추가
    - 하이브리드 암호화(대칭키를 개인키로 복호화, 대칭키로 파일 복호화)
    - 파일 병합 실패시 롤백 로직 추가
    - 파일 복호화 성공시 암호화된 파일 삭제 로직 추가

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const path = require('path');
+
 const { changeFilename, encryptAndSplitFile } = require('./file-processing/writer');
 const { renameFilesAndCreateMapping, uploadFiles, saveMappingDataJsonPostgreSQL } = require('./file-processing/middle');
-const { getFileMappingInfo, sortFiles, mergeFiles, processDecryptedFilename } = require('./file-processing/reader');
-const { compareFileHash } = require('./utils/comparefile');
+const { mergeAndDecryptFile } = require('./file-processing/reader');
+const { calculateFileHash, compareFileHash, compareMultipleFiles } = require('./utils/hashFunctions');
 
-// 하드코딩
-const fs = require('fs').promises;
-const newLocation = '/Users/mac/Documents/split_file/internet';
 const publicKeyPath = path.join(__dirname, 'key', 'public_key.pem');
 
 // 업로드 된 파일 위치
@@ -14,37 +12,39 @@ const folderPath = path.join(__dirname, 'uploadfile');
 
 const processFiles = async () => {
   try {
-    // 암호화된 dummyfile 파일명 초기화
-    const desEncryptedFilePath = path.join(folderPath, 'c14b8211d0b37c6a89a1c448c59c028f');
-    changeFilename(desEncryptedFilePath, 'dummyfile')
-    console.log('dummyfile로 파일명 변경 완료')
-
     // 원본 파일 경로와 분할할 조각 수 설정
-    const originalFilePath = path.join(folderPath, 'dummyfile');
+    const originalFilePath = path.join(folderPath, 'dummyfile.mp4');
     const splitCount = 100; // 예시 분할 조각 수
 
     // 파일 암호화 및 분할 실행
-    const { encryptedSymmetricKey, originalFileNames, splitFilesPath } = await encryptAndSplitFile(originalFilePath, publicKeyPath, splitCount);
-    console.log('파일이 성공적으로 암호화되고 분할되었습니다.');
+    const { encryptedPassword, originalFileNames, splitFilesPath } = await encryptAndSplitFile(originalFilePath, publicKeyPath, splitCount);
+    // console.log('파일이 성공적으로 암호화되고 분할되었습니다.');
 
     // 파일 이름 변경 및 매핑 생성
     const { renamedFilePaths, splitFileOrderMapping, desEncryptedFileName } = await renameFilesAndCreateMapping(originalFileNames, splitFilesPath);
-    console.log('파일 이름 변경 및 매핑 정보 생성 완료.');
+    // console.log('파일 이름 변경 및 매핑 정보 생성 완료.');
 
     //---------------------------------------------------------
-    // 잠시 테스트를 위해 주석 처리
+    // File Upload
     //---------------------------------------------------------
     // // 파일 업로드
     // const uploadUrl = 'http://localhost:3000/upload'; // 업로드할 URL 예시
     // await uploadFiles(renamedFilePaths, uploadUrl);
     // console.log('파일 업로드가 완료되었습니다.');
 
-    // 하드코딩
+    //---------------------------------------------------------
+    // File Move
+    //---------------------------------------------------------
     /**
      * 주어진 파일 경로 목록의 파일들을 새 위치로 이동.
      * @param {string[]} filePaths - 이동할 파일들의 경로.
      * @param {string} newLocation - 파일들을 이동할 새 위치.
      */
+
+    // 하드코딩
+    const fsp = require('fs').promises;
+    const newLocation = '/Users/mac/Documents/split_file/internet';
+
     const moveFilesToNewLocation = async (filePaths, newLocation) => {
       const movedFilePaths = [];
       try {
@@ -53,12 +53,12 @@ const processFiles = async () => {
 
           // 새 위치에 상위 폴더를 생성.
           const newParentFolderPath = path.join(newLocation, path.basename(parentFolderPath));
-          await fs.mkdir(newParentFolderPath, { recursive: true });
+          await fsp.mkdir(newParentFolderPath, { recursive: true });
 
           for (const filePath of filePaths) {
               const fileName = path.basename(filePath);
               const newFilePath = path.join(newParentFolderPath, fileName);
-              await fs.rename(filePath, newFilePath);
+              await fsp.rename(filePath, newFilePath);
               movedFilePaths.push(newFilePath); // 배열에 이동된 파일 경로 추가
           }
           return movedFilePaths; // 이동된 파일 경로들의 배열 반환
@@ -70,9 +70,8 @@ const processFiles = async () => {
     const movedFilePaths = await moveFilesToNewLocation(renamedFilePaths, newLocation);
     //
 
-    
     // 업로드 후 매핑 데이터 저장
-    await saveMappingDataJsonPostgreSQL(desEncryptedFileName, splitFileOrderMapping, encryptedSymmetricKey);
+    await saveMappingDataJsonPostgreSQL(desEncryptedFileName, splitFileOrderMapping, encryptedPassword);
 
     // 하드 코딩
     return movedFilePaths;
@@ -87,25 +86,22 @@ const afterProcessFiles = async (movedFilePaths) => {
   try {
     // getFileMappingInfo 함수 실행
     // 하드 코딩
-    const encryptedFilename = 'c14b8211d0b37c6a89a1c448c59c028f'; // 사용자가 입력한 파일명을 대칭키로 암호화
-    const db_mappingInfo = await getFileMappingInfo(encryptedFilename);
-    // console.log('파일 매핑 정보:', db_mappingInfo);
+    const encryptedFilename = '0be8366e87a3f33ae2d2ebb5fa9bfb21'; // 사용자가 입력한 파일명을 대칭키로 암호화
 
-    const sortedFiles = sortFiles(movedFilePaths, db_mappingInfo)
-    // console.log('정렬된 파일들:', sortedFiles);
+    await mergeAndDecryptFile(encryptedFilename, movedFilePaths)
 
-    // 하드코딩
-    const outputPath = '/Users/mac/Documents/split_file/output';
-    const mergedFilePath = await mergeFiles(sortedFiles, outputPath, encryptedFilename)
-    // console.log(mergedFilePath)
-    // console.log(folderPath)
+    // 비교하려는 파일 경로들
+    const filesToCompare = [
+      // '/Users/mac/Documents/split_file/uploadfile/0be8366e87a3f33ae2d2ebb5fa9bfb21',
+      '/Users/mac/Documents/split_file/result/dummyfile.mp4',
+      // '/Users/mac/Documents/split_file/encryptedfile/0be8366e87a3f33ae2d2ebb5fa9bfb21',
+      // '/Users/mac/Documents/split_file/output/0be8366e87a3f33ae2d2ebb5fa9bfb21',
+      '/Users/mac/Documents/split_file/dummyfile.mp4'
+    ];
 
-    // 하드코딩
-    const areFileSame = await compareFileHash("/Users/mac/Documents/split_file/uploadfile/c14b8211d0b37c6a89a1c448c59c028f", mergedFilePath)
-
-    // 12. 파일명 복호화
-    const decryptedFilePath = await processDecryptedFilename(mergedFilePath);
-
+    // 파일 비교 시작
+    compareMultipleFiles(filesToCompare);
+    
   } catch (error) {
     console.error('afterProcessFiles error:', error);
   }
--- file-processing/reader.js
+++ file-processing/reader.js
@@ -1,47 +1,78 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = require('fs').promises;
 const axios = require('axios');
 const path = require('path');
 const splitFile = require('split-file');
 const crypto = require('crypto');
 
 const { logger } = require('../utils/logger');
-const FileMappingJson = require('../models/postgreSQLModels');
-const { isDirectory } = require('../utils/pathUtils');
-const { changeFilename } = require('./writer')
+const { calculateFileHash, compareFileHash, compareMultipleFiles } = require('../utils/hashFunctions');
+const { deleteFolderAndFiles } = require('../utils/rollbackUtils');
 
+const FileMappingJson = require('../models/postgreSQLModels');
+const { changeFilename } = require('./writer');
 
 //=============================================================================================
 // 파일 병합
 //=============================================================================================
 
-//---------------------------------------------------------
-// 8. fileOrderMapping 정보 조회
-//---------------------------------------------------------
-/**
- * 주어진 암호화된 파일명에 대한 파일 매핑 정보를 검색합니다.
- * @param {string} encryptedFilename - 검색할 암호화된 파일의 이름
- * @returns {Promise<Object|null>} 매핑 정보 객체를 반환하는 프로미스
- */
-async function getFileMappingInfo(encryptedFilename) {
+// 파일 매핑 정보 및 암호화된 대칭키를 암호화된 파일명으로 검색
+async function getFileMappingData(encryptedFilename) {
     try {
-        const mappingInfo = await FileMappingJson.findOne({
+        // del
+        logger.info(`Retrieving file mapping data for: ${encryptedFilename}`);
+        // 데이터베이스에서 파일 데이터 검색
+        const fileData = await FileMappingJson.findOne({
             where: { encrypted_filename: encryptedFilename }
         });
 
-        if (mappingInfo) {
-            // console.log("잘 통과함")
-            return mappingInfo.mapping_info;
-        } else {
-            console.log(`${encryptedFilename} 파일 존재하지 않음`)
+        // 데이터가 없으면 null 반환
+        if (!fileData) {
+            console.log(`${encryptedFilename}는 데이터베이스에 존재하지 않습니다.`);
             return null;
         }
+
+        // del
+        logger.info(`Mapping data retrieved for: ${encryptedFilename}`);
+
+        // 단일 객체로 모든 필요한 데이터 반환
+        return {
+            mappingInfo: fileData.mapping_info, // 매핑 정보
+            encryptedPassword: fileData.encrypted_symmetric_key // base64 인코딩된 암호화된 대칭키
+        };
     } catch (error) {
-        console.error('파일 매핑 정보 조회 중 오류 발생:', error);
-        return null;
+        logger.error(`파일 매핑 데이터 검색 중 오류 발생:${encryptedFilename}`, error);
+        throw error;
     }
 }
 
 //---------------------------------------------------------
+// 8. fileOrderMapping 정보 조회
+//---------------------------------------------------------
+// 파일 매핑 정보 사용
+async function useMappingInfo(encryptedFilename) {
+    try {
+        logger.info(`Using mapping info for: ${encryptedFilename}`);
+        const fileData = await getFileMappingData(encryptedFilename);
+        if (!fileData) {
+
+            // del
+            logger.error(`No file data found for: ${encryptedFilename}`);
+
+            return null;
+        }
+        return fileData.mappingInfo;
+    } catch (error) {
+
+        // del
+        logger.error(`Error using mapping info for: ${encryptedFilename}`, error);
+
+        throw error;
+    }
+}
+
+//---------------------------------------------------------
+// TODO: 다운로드 기능 구현, 업로드 코드와 비슷한 구조를 가지기.
 // 9. 분할된 파일들을 다운로드 (아직 구현되지 않음)
 // Promise.all()을 사용하여 병렬로 다운로드
 //---------------------------------------------------------
@@ -57,7 +88,7 @@ async function downloadFiles(urls, destFolder) {
             const fileName = path.basename(url); // URL에서 파일 이름 추출
             const filePath = path.join(destFolder, `${index}-${fileName}`); // 파일 이름 앞에 인덱스를 추가하여 고유한 파일 이름을 보장합니다.
             const response = await axios({ url, responseType: 'stream' });
-            const writer = fs.createWriteStream(filePath);
+            const writer = fsp.createWriteStream(filePath);
             
             response.data.pipe(writer);
 
@@ -78,26 +109,37 @@ async function downloadFiles(urls, destFolder) {
 //---------------------------------------------------------
 // 10. 파일 정렬
 //---------------------------------------------------------
-const sortFiles = (filesPath, mappingInfo) => {
-    // 다운로드 받은 파일들을 mappingInfo를 기반으로 정렬
-    const sortedFiles = filesPath.sort((a, b) => mappingInfo[a] - mappingInfo[b]);
+async function sortFilesByMappingInfo(filesPath, mappingInfo) {
+    // 매핑 정보를 배열로 변환하고 원본 파일 순서를 기반으로 정렬합니다.
+    const sortedFilePaths = Object.entries(mappingInfo)
+        // 각 항목을 원본 인덱스(각 항목의 값 부분)를 기준으로 정렬합니다.
+        .sort((a, b) => a[1] - b[1])
+        // 정렬된 항목을 해당 파일 경로로 다시 매핑합니다.
+        .map(([newFileName]) => filesPath.find(path => path.includes(newFileName)));
 
-    return sortedFiles;
-};
+    // 정렬된 파일 경로 중 일부가 누락되었는지 확인합니다.
+    if (sortedFilePaths.some(path => path === undefined)) {
+        throw new Error('일부 정렬된 파일 경로가 누락되었습니다.');
+    }
+
+    return sortedFilePaths;
+}
 
 //---------------------------------------------------------
 // 11. 파일 병합
 //---------------------------------------------------------
-async function mergeFiles(filesPath, outputPath, mergedFileName) {
-    const mergedFilePath = path.join(outputPath, mergedFileName);
+async function mergeSortedFiles(sortedFilePaths, outputPath, mergedFileName) {
     try {
-        // 파일 합치기
-        await splitFile.mergeFiles(filesPath, mergedFilePath);
-        console.log('파일 합치기 완료');
+        const mergedFilePath = path.join(outputPath, mergedFileName);
+
+        // 정렬된 파일들을 병합합니다.
+        await splitFile.mergeFiles(sortedFilePaths, mergedFilePath);
+        logger.info(`파일 병합 성공: ${mergedFilePath}`);
         return mergedFilePath;
-    } catch(err) {
-        console.error('파일 병합 도중 오류 발생:', err);
-        throw err;
+    } catch (error) {
+        logger.error('파일 병합 중 오류 발생:', error);
+        deleteFolderAndFiles(outputPath);
+        throw error;
     }
 }
 
@@ -107,11 +149,11 @@ async function mergeFiles(filesPath, outputPath, mergedFileName) {
 // 1. 서버 2. db
 // 계정당 발급? 통합으로 1개만 발급?(그나마 얘가 합당하지 않나?)
 //---------------------------------------------------------
-// AES 암호화 설정
-const AES_algorithm = 'aes-192-cbc';
-const AES_password = '비밀번호';
-const AES_key = crypto.scryptSync(AES_password, 'salt', 24);
-const AES_iv = Buffer.alloc(16, 0);
+// AES 암호화 설정 (파일명용)
+const AES_algorithm_filename = 'aes-192-cbc';
+const AES_password_filename = '파일명용비밀번호';
+const AES_key_filename = crypto.scryptSync(AES_password_filename, 'saltForFilename', 24);
+const AES_iv_filename = Buffer.alloc(16, 0); // 초기화 벡터
 
 /**
  * 파일명을 AES 복호화하여 반환
@@ -120,11 +162,23 @@ const AES_iv = Buffer.alloc(16, 0);
  * @return {string} 복호화된 파일명
  */
 function decryptFilename(encryptedFileName) {
-    const decipher = crypto.createDecipheriv(AES_algorithm, AES_key, AES_iv);
-    let decrypted = decipher.update(encryptedFileName, 'hex', 'utf8');
-    // hex -> utf8 인코딩
-    decrypted += decipher.final('utf8');
-    return decrypted;
+    try {
+        // del
+        logger.info(`Decrypting filename: ${encryptedFileName}`);
+
+        const decipher = crypto.createDecipheriv(AES_algorithm_filename, AES_key_filename, AES_iv_filename);
+        let decrypted = decipher.update(encryptedFileName, 'hex', 'utf8');
+        // hex -> utf8 인코딩
+        decrypted += decipher.final('utf8');
+
+        // del
+        logger.info(`Decrypted filename: ${decrypted}`);
+
+        return decrypted;
+    } catch (error) {
+        logger.error(`Error decrypting filename: ${encryptedFileName}`, error);
+        throw error;
+    }
 }
 
 /**
@@ -133,31 +187,147 @@ function decryptFilename(encryptedFileName) {
  * @param {string} encryptedFilePath 처리할 파일의 암호화된 경로
  * @return {string} 변경된 파일의 경로
  */
-async function processDecryptedFilename(encryptedFilePath) {
-    const encryptedFileName = path.basename(encryptedFilePath);
-    let decryptedFileName, decryptedFilePath;
-
+function processDecryptedFilename(encryptedFilePath) {
     try {
-        decryptedFileName = decryptFilename(encryptedFileName);
-        decryptedFilePath = await changeFilename(encryptedFilePath, decryptedFileName);
+        const encryptedFileName = path.basename(encryptedFilePath);
 
-        logger.info(`파일 처리 완료: ${decryptedFilePath}`);
+        // del
+        logger.info(`Processing decrypted filename for: ${encryptedFileName}`);
+
+        const decryptedFileName = decryptFilename(encryptedFileName);
+        const decryptedFilePath = changeFilename(encryptedFilePath, decryptedFileName);
+        logger.info(`파일명 복호화 완료: ${decryptedFilePath}`);
         return decryptedFilePath;
     } catch (error) {
-        logger.error(`파일 처리 중 오류 발생: ${error.message}`);
+        logger.error(`파일명 복호화 중 오류 발생: ${error.message}`);
         throw error;
     }
 }
 
 //---------------------------------------------------------
-// 13. 파일 복호화 (아직 구현되지 않음)
+// 13. 파일 복호화
 //---------------------------------------------------------
+
+// 암호화된 대칭키 사용
+async function useEncryptedSymmetricKey(encryptedFilename) {
+    const fileData = await getFileMappingData(encryptedFilename);
+    if (fileData) {
+        // 암호화된 대칭키에서 공백과 줄바꿈 제거
+        const formattedEncryptedKey = fileData.encryptedPassword.split(' ').join('').split('\n').join('').split('\r').join('');
+
+        // base64 형식의 저장된 대칭키를 변환하여 사용
+        const encryptedPassword = Buffer.from(formattedEncryptedKey, 'base64');
+
+        return encryptedPassword;
+    }
+}
+
+/**
+ * 암호화된 대칭키를 개인키로 복호화.
+ * @param {Buffer} encryptedPassword - 암호화된 대칭키
+ * @param {string} privateKeyPath - 개인키 파일 경로
+ * @returns {Promise<Buffer>} - 복호화된 대칭키
+ */
+async function decryptSymmetricKey(encryptedPassword, privateKeyPath) {
+    try {
+        const privateKey = await fsp.readFile(privateKeyPath, 'utf8');
+        const decryptedPassword = crypto.privateDecrypt(
+            {
+                key: privateKey,
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: "sha256",
+            },
+            encryptedPassword
+        );
+        logger.info('대칭키 복호화 성공');
+
+        return decryptedPassword; // 복호화된 AES_password 반환
+    } catch (error) {
+        logger.error(`대칭키 복호화 중 오류 발생: ${error}`);
+        throw error;
+    }
+}
+
+// 복호화된 대칭키로 파일 복호화
+/**
+ * 복호화된 대칭키로 암호화된 파일 복호화.
+ * @param {string} encryptedFilePath - 암호화된 파일의 경로
+ * @param {string} decryptedFilePath - 복호화된 파일이 저장될 경로
+ * @param {Buffer} symmetricKey - 복호화에 사용될 복호화된 대칭키
+ */
+async function decryptFileWithSymmetricKey(encryptedFilePath, decryptedFilePath, decryptedPassword) {
+    return new Promise(async (resolve, reject) => {
+
+        // del
+        logger.info(`Decrypting file: ${encryptedFilePath}`);
+
+        const AES_password_file = decryptedPassword.toString('hex')
+        AES_password_file.split(' ').join('').split('\n').join('').split('\r').join('');
+        const AES_key_file = crypto.scryptSync(AES_password_file, 'saltForFile', 32); // 복호화된 AES_password로부터 AES_key 생성
+        const AES_iv_file = Buffer.alloc(16, 0); // 파일 복호화를 위한 초기화 벡터
+        
+        try {
+            // 읽기, 복호화, 쓰기를 위한 스트림 생성
+            const readStream = fs.createReadStream(encryptedFilePath);
+            const writeStream = fs.createWriteStream(decryptedFilePath);
+            const decipher = crypto.createDecipheriv('aes-256-cbc', AES_key_file, AES_iv_file);
+            
+            // 스트림 파이프 연결
+            readStream.pipe(decipher).pipe(writeStream);
+
+            // 스트림들에 대한 에러 핸들링
+            const errorHandler = (source) => (err) => {
+                readStream.close();
+                writeStream.close();
+                reject(`파일 복호화 도중 에러 발생 ${source}: ${err}`);
+            };
+
+            readStream.on('error', errorHandler('Read Stream'));
+            writeStream.on('error', errorHandler('Write Stream'));
+            decipher.on('error', errorHandler('Decipher Stream'));
+
+            // writeStream의 'finish' 이벤트 처리
+            writeStream.on('finish', () => {
+                logger.info(`파일 복호화 성공: ${decryptedFilePath}`);
+                
+                // 정상적으로 복호화된 파일이 생성되었으므로 원본 파일 삭제
+                deleteFolderAndFiles(encryptedFilePath);
+                resolve();
+            });
+        } catch (error) {
+            reject(`파일 복호화 실패: ${encryptedFilePath}`, error);
+        }
+    });
+}
+
+async function mergeAndDecryptFile(encryptedFilename, movedFilePaths){
+    const db_mappingInfo = await useMappingInfo(encryptedFilename);
+
+    // 하드코딩
+    const outputPath = '/Users/mac/Documents/split_file/output';
+
+    // 10. 파일 정렬
+    const sortedFilePaths = await sortFilesByMappingInfo(movedFilePaths, db_mappingInfo);
+
+    // 11. 파일 병합
+    const mergedFilePath = await mergeSortedFiles(sortedFilePaths, outputPath, encryptedFilename);
+
+    const encryptedPassword = await useEncryptedSymmetricKey(encryptedFilename)
+
+    // compareFileHash(mergedFilePath, "/Users/mac/Documents/split_file/encryptedfile/0be8366e87a3f33ae2d2ebb5fa9bfb21")
+
+    const privateKeyPath = "/Users/mac/Documents/split_file/key/private_key.pem"
+    const decryptedPassword = await decryptSymmetricKey(encryptedPassword, privateKeyPath)
+
+    const decryptedFilePath = mergedFilePath.replace('/output/', '/result/');
+    await decryptFileWithSymmetricKey(mergedFilePath, decryptedFilePath, decryptedPassword)
+
+    // 12. 파일명 복호화
+    const decryptedFileNamePath = processDecryptedFilename(decryptedFilePath);
+}
 
 
 module.exports = {
-    getFileMappingInfo,
-    sortFiles,
-    mergeFiles,
-    processDecryptedFilename
+    mergeAndDecryptFile
     //... (8~13번 기능의 export)
 };
