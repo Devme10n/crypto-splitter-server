ce5d5809578004447f406e7f2c0bd998f9abd5b2
max990624 <max990624@naver.com>
2024-03-23 02:57:16 +0900

    feat: 대칭키로 파일 암호화 및 공개키로 파일 암호화, 파일 + 대칭키 업로드 기능 구현
    - aes-256-cbc 알고리즘과 password, salt, iv를 매개변수로 하여 대칭키 생성
    - 생성한 대칭키로 파일 암호화
    - 대칭키를 공개키로 암호화
    - 암호화된 대칭키와 암호화된 파일을 서버로 업로드

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,28 @@
-// src/App.js
 import React, { useState } from 'react';
-import FileHandler from './components/FileHandler';
+import FileUploader from './components/FileUploader';
 import CryptoFunctions from './components/CryptoFunctions';
-import { generateRSAKeys } from './utils/crypto';
 
 function App() {
   const [file, setFile] = useState(null);
-  const [downloadUrl, setDownloadUrl] = useState('');
   const [publicKey, setPublicKey] = useState(null);
   const [privateKey, setPrivateKey] = useState(null);
   const [symmetricKey, setSymmetricKey] = useState(null);
+  const [downloadUrl, setDownloadUrl] = useState(null);
 
-  // 파일 선택 핸들러
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-  // 키 생성 핸들러
-  const handleKeyGeneration = async () => {
-    console.log('RSA 키 생성 중...'); // 키 생성 시작 로그
-    const keys = await generateRSAKeys();
-    setPublicKey(keys.publicKey);
-    setPrivateKey(keys.privateKey);
-    console.log('RSA 키 생성 완료'); // 키 생성 완료 로그
-  };
-
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleKeyGeneration}>Generate Keys</button>
-      <FileHandler 
+      <CryptoFunctions setPublicKey={setPublicKey} setPrivateKey={setPrivateKey} />
+      <FileUploader 
         file={file}
         symmetricKey={symmetricKey} 
         setDownloadUrl={setDownloadUrl} 
         publicKey={publicKey} 
         privateKey={privateKey}
       />
-      <CryptoFunctions 
-        setPublicKey={setPublicKey}
-        setPrivateKey={setPrivateKey}
-        setSymmetricKey={setSymmetricKey}
-        publicKey={publicKey}
-        privateKey={privateKey}
-        symmetricKey={symmetricKey}
-      />
-      {/* {downloadUrl && <a href={downloadUrl} download="encrypted_file">Download File</a>} */}
     </div>
   );
 }
--- frontend/src/utils/crypto.js
+++ frontend/src/utils/crypto.js
@@ -1,5 +1,7 @@
 // src/utils/crypto.js
 
+import axios from 'axios';
+
 // TODO: RSA 키가 없는 경우 최초 1회에만 키를 생성, 그 이후에는 로컬 스토리지에서 가져오도록 수정, 키 생성 후 로컬 스토리지에 저장
 // RSA 키 생성 함수
 export const generateRSAKeys = async () => {
@@ -18,133 +20,87 @@ export const generateRSAKeys = async () => {
     return keyPair;
 };
 
-// AES-GCM 대칭 키를 사용한 데이터 암호화 함수
-export const encryptDataWithAES = async (arrayBuffer, symmetricKey) => {
-    // AES-GCM 암호화에 필요한 초기화 벡터 생성
-    const iv = window.crypto.getRandomValues(new Uint8Array(12));
-    console.log("AES-GCM 암호화를 위한 IV:", iv);
-    // 데이터 암호화 시도
-    try {
-        const encryptedData = await window.crypto.subtle.encrypt(
-            {
-                name: "AES-GCM",
-                iv,
-            },
-            symmetricKey,
-            arrayBuffer
-        );
-        // 암호화 성공 로그 출력
-        console.log("데이터 암호화 성공");
-        // 암호화된 데이터 및 IV 반환
-        return { encryptedData, iv };
-    } catch (error) {
-        // 암호화 실패 시 오류 로그 출력
-        console.error("데이터 암호화 실패:", error);
-        throw error;
-    }
-};
-  
-// AES-GCM을 사용한 대칭키로 파일 데이터를 복호화하는 함수
-export const decryptFile = async (encryptedData, symmetricKey) => {
-    if (encryptedData && symmetricKey) {
-      const iv = new Uint8Array(12); // 암호화에 사용된 동일한 초기화 벡터(IV).
-      try {
-        const decryptedData = await window.crypto.subtle.decrypt(
-          { name: "AES-GCM", iv },
-          symmetricKey,
-          encryptedData
-        );
-        console.log("파일 데이터 복호화 성공");
-        return decryptedData; // 복호화된 데이터를 반환
-      } catch (error) {
-        console.error("파일 데이터 복호화 실패:", error);
-        throw error; // 에러를 다시 발생시키기
-      }
-    }
-  };
-  
-  // RSA 공개키를 사용하여 대칭키를 암호화하는 함수
-  export const exportAndEncryptSymmetricKey = async (publicKey, symmetricKey) => {
-    if (publicKey && symmetricKey) {
-      try {
-        const exportedKey = await window.crypto.subtle.exportKey("raw", symmetricKey);
-        const encryptedSymmetricKey = await window.crypto.subtle.encrypt(
-          { name: "RSA-OAEP" },
-          publicKey,
-          exportedKey
-        );
-        console.log("대칭키 암호화 성공");
-        return encryptedSymmetricKey; // 암호화된 대칭키를 반환
-      } catch (error) {
-        console.error("대칭키 암호화 실패:", error);
-        throw error; // 에러를 다시 발생시키기
-      }
-    }
-  };
-  
-  // RSA 개인키를 사용하여 암호화된 대칭키를 복호화하는 함수
-  export const decryptSymmetricKey = async (privateKey, encryptedSymmetricKey) => {
-    if (privateKey && encryptedSymmetricKey) {
-      try {
-        const decryptedKey = await window.crypto.subtle.decrypt(
-          { name: "RSA-OAEP" },
-          privateKey,
-          encryptedSymmetricKey
-        );
-        const importedKey = await window.crypto.subtle.importKey(
-          "raw",
-          decryptedKey,
-          { name: "AES-GCM" },
-          true,
-          ["encrypt", "decrypt"]
-        );
-        console.log("대칭키 복호화 및 가져오기 성공");
-        return importedKey; // 복호화 및 가져온 대칭키를 반환
-      } catch (error) {
-        console.error("대칭키 복호화 실패:", error);
-        throw error; // 에러를 다시 발생시키기
-      }
-    }
-  };
+export async function generateKeyAndIv() {
+  // 안전한 랜덤 비밀번호 생성
+  const password = window.crypto.getRandomValues(new Uint8Array(16));
+  const AES_password_file = Array.from(password, byte => ('00' + byte.toString(16)).slice(-2)).join('');
+  console.log('AES_password_file:', AES_password_file); // 로그 추가
 
-// AES-GCM을 사용하여 대칭키를 생성하는 함수
-export const generateSymmetricKey = async () => {
-    return await window.crypto.subtle.generateKey(
-        {
-            name: "AES-GCM",
-            length: 256, // 혹은 보안 요구에 따라 128 선택 가능
-        },
-        true, // 키가 추출 가능한지 여부 (즉, exportKey에서 사용할 수 있는지)
-        ["encrypt", "decrypt"] // "encrypt", "decrypt", "wrapKey", "unwrapKey" 중 사용할 수 있음
-    );
-};
+  // salt 생성
+  const salt = 'saltForFile';
+  const encoder = new TextEncoder();
+  const saltUint8Array = encoder.encode(salt);
 
-// 대칭키를 사용하여 데이터를 암호화하는 함수
-export const encryptData = async (symmetricKey, data) => {
-    const iv = window.crypto.getRandomValues(new Uint8Array(12)); // AES-GCM용 초기화 벡터
+  // 파일용 대칭키 생성
+  const AES_key_file = await window.crypto.subtle.importKey(
+      'raw',
+      encoder.encode(AES_password_file),
+      { name: 'PBKDF2' },
+      false,
+      ['deriveKey']
+  );
+  console.log('AES_key_file:', AES_key_file); // 로그 추가
 
-    const encryptedData = await window.crypto.subtle.encrypt(
-        {
-            name: "AES-GCM",
-            iv,
-        },
-        symmetricKey, // generateSymmetricKey에서 생성된 대칭키
-        data // 암호화하려는 데이터의 ArrayBuffer
-    );
+  const derivedKey = await window.crypto.subtle.deriveKey(
+      {
+          name: 'PBKDF2',
+          salt: saltUint8Array,
+          iterations: 1000,
+          hash: 'SHA-256'
+      },
+      AES_key_file,
+      { name: 'AES-CBC', length: 256 },
+      false,
+      ['encrypt', 'decrypt']
+  );
+  console.log('derivedKey:', derivedKey); // 로그 추가
 
-    return { encryptedData, iv }; // 암호화된 데이터와 IV 반환
-};
+  // 파일용 초기화 벡터 생성
+  const AES_iv_file = window.crypto.getRandomValues(new Uint8Array(16));
+  console.log('AES_iv_file:', AES_iv_file); // 로그 추가
 
-// 대칭키를 사용하여 데이터를 복호화하는 함수
-export const decryptData = async (symmetricKey, encryptedData, iv) => {
-    const decryptedData = await window.crypto.subtle.decrypt(
-        {
-            name: "AES-GCM",
-            iv,
-        },
-        symmetricKey, // generateSymmetricKey에서 생성된 대칭키
-        encryptedData // 복호화하려는 데이터의 ArrayBuffer
-    );
+  return { AES_password_file, derivedKey, AES_iv_file };
+}
 
-    return new Uint8Array(decryptedData); // 복호화된 데이터를 사용 가능한 형식으로 변환
-};
+
+// TODO: 대용량 파일(아마 2기가 이상)의 경우 server-side한 선택지를 제공하기
+export async function encryptAndUploadFile(file, derivedKey, iv, publicKey) {
+  // 파일을 암호화합니다.
+  const arrayBuffer = await file.arrayBuffer();
+  const encryptedData = await window.crypto.subtle.encrypt(
+    {
+      name: 'AES-CBC',
+      iv: iv,
+    },
+    derivedKey,
+    arrayBuffer
+  );
+  console.log('encryptedData:', encryptedData); // 로그 추가
+
+  // iv를 암호화된 데이터 앞에 붙입니다.
+  const ivAndEncryptedData = new Uint8Array(iv.byteLength + encryptedData.byteLength);
+  ivAndEncryptedData.set(new Uint8Array(iv), 0);
+  ivAndEncryptedData.set(new Uint8Array(encryptedData), iv.byteLength);
+
+  // 대칭키를 공개키로 암호화합니다.
+  const encryptedKey = await window.crypto.subtle.encrypt(
+    {
+      name: 'RSA-OAEP',
+    },
+    publicKey,
+    derivedKey
+  );
+  console.log('encryptedKey:', encryptedKey); // 로그 추가
+
+  // 암호화된 파일과 대칭키를 서버로 업로드합니다.
+  const formData = new FormData();
+  formData.append('file', new Blob([ivAndEncryptedData]), file.name);
+  formData.append('key', new Blob([new Uint8Array(encryptedKey)]));
+  const response = await axios.post('https://example.com/api/upload', formData);
+
+  if (response.status === 200) {
+    console.log('File uploaded successfully');
+  } else {
+    console.error('File upload failed', response);
+  }
+}
