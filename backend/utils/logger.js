cd42d0906cb3255cca071c135160191029772f4f
max990624 <max990624@naver.com>
2024-04-06 22:53:38 +0900

    feat: 로그 메시지에 호출자 정보를 추가하는 기능을 구현

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,10 +1,36 @@
 const winston = require('winston');
+const path = require('path');
+
+const getCallerInfo = function() {
+  const originalFunc = Error.prepareStackTrace;
+
+  Error.prepareStackTrace = function(_, stack) {
+    return stack;
+  };
+
+  const err = new Error();
+  Error.captureStackTrace(err, this);
+
+  const caller = err.stack[11];
+
+  Error.prepareStackTrace = originalFunc;
+
+  return {
+    filePath: path.basename(caller.getFileName()),
+    functionName: caller.getFunctionName(),
+    lineNumber: caller.getLineNumber(),
+  };
+};
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
+    winston.format.printf(info => {
+      const callerInfo = getCallerInfo();
+      return `${info.timestamp} [${callerInfo.filePath}:${callerInfo.lineNumber} (${callerInfo.functionName})] ${info.level}: ${info.message}`;
+    })
   ),
   transports: [
     new winston.transports.File({ filename: 'backend/error.log', level: 'error' }),
