7750d664350d8b3371e0288fa9c3df1990f8ae04
max990624 <max990624@naver.com>
2024-04-30 22:58:47 +0900

    refactor: 로그 파일 경로 수정
     - backend/ -> logs/

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -33,8 +33,8 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    new winston.transports.File({ filename: 'backend/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'backend/combined.log' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
     new winston.transports.Console({ level: 'info' })
   ]
 });
