97589a8f1bff29b905082eae594ae490ea863320
max990624 <max990624@naver.com>
2024-05-10 20:51:31 +0900

    feat: add dockerfile
    - dockerfile, dockerignore 추가
    - sequelize production 설정 변경
    - models/postgreSQLModels.js, 환경 변수 확인 함수 추가
    - process.env를 활용한 환경 변수 참조로 변경

--- backend/models/postgreSQLModels.js
+++ backend/models/postgreSQLModels.js
@@ -1,10 +1,35 @@
+// server/backend/models/postgreSQLModels.js
 const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config({ path: '../../env/server.env' });
 
-const sequelize = new Sequelize('database_development', 'mac', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: console.log
-});
+// 환경 변수 확인
+function checkEnvironmentVariables() {
+    const envVars = ['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST'];
+    const missingVars = envVars.filter(varName => !process.env[varName]);
+
+    if (missingVars.length > 0) {
+        console.error('Missing required environment variables:', missingVars.join(', '));
+        process.exit(1); // Exit the application with an error code
+    }
+}
+
+checkEnvironmentVariables();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,    // Environment variable for database name
+    process.env.DB_USER,    // Environment variable for database username
+    process.env.DB_PASSWORD, // Environment variable for database password
+    {
+        host: process.env.DB_HOST,    // Environment variable for database host
+        dialect: 'postgres',
+        logging: (msg) => console.log(msg)
+    }
+);
+
+// 데이터베이스 연결 테스트
+sequelize.authenticate()
+    .then(() => console.log('데이터베이스에 성공적으로 연결되었습니다.'))
+    .catch(err => console.error('데이터베이스에 연결할 수 없습니다:', err));
 
 // 모델 정의
 const FileMappingJson = sequelize.define('file_mapping_json', {
